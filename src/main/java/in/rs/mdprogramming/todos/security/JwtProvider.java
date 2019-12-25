047e88f08bbf5b50287ecff1588215dea7492a10
dulleo <dulleo@gmail.com>
2019-12-25 08:43:15 +0100

    implementation added

--- src/main/java/in/rs/mdprogramming/todos/security/JwtProvider.java
+++ src/main/java/in/rs/mdprogramming/todos/security/JwtProvider.java
@@ -1,4 +1,29 @@
 package in.rs.mdprogramming.todos.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+
+import java.util.Date;
+
 public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private Integer jwtExpiration;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        UserPrinciple userPrincipal = (UserPrinciple) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpiration*1000))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
 }
