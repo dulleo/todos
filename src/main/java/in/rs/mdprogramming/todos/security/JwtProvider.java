e09e36ad43705d8df7e3bdb2ad693d7e3e5e5354
dulleo <dulleo@gmail.com>
2019-12-25 09:15:37 +0100

    @Component annotation added

--- src/main/java/in/rs/mdprogramming/todos/security/JwtProvider.java
+++ src/main/java/in/rs/mdprogramming/todos/security/JwtProvider.java
@@ -4,12 +4,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 /**
  * This is class that generates a JWT token
  */
+@Component
 public class JwtProvider {
 
     @Value("${jwt.secret}")
