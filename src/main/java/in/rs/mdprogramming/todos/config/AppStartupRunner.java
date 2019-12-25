1829c94d1ee50a49bb7717b44b6514d28384ca37
dulleo <dulleo@gmail.com>
2019-12-25 08:25:38 +0100

    add roles if table is empty impl

--- src/main/java/in/rs/mdprogramming/todos/config/AppStartupRunner.java
+++ src/main/java/in/rs/mdprogramming/todos/config/AppStartupRunner.java
@@ -1,4 +1,40 @@
 package in.rs.mdprogramming.todos.config;
 
-public class AppStartupRunner {
+import in.rs.mdprogramming.todos.model.Role;
+import in.rs.mdprogramming.todos.model.RoleName;
+import in.rs.mdprogramming.todos.repository.RoleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ *
+ */
+@Component
+public class AppStartupRunner implements ApplicationRunner {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+        List<Role> roles = roleRepository.findAll();
+
+        if(roles.isEmpty()) {
+            Role admin = new Role();
+            admin.setName(RoleName.ROLE_ADMIN);
+            roles.add(admin);
+
+            Role user = new Role();
+            user.setName(RoleName.ROLE_USER);
+            roles.add(user);
+
+            roleRepository.saveAll(roles);
+        }
+    }
 }
