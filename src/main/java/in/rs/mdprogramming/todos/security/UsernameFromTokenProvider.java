e7552b3f39e1f4dfdadbd49cd9d8631fdfea2260
dulleo <dulleo@gmail.com>
2019-12-25 08:40:18 +0100

    implementation added

--- src/main/java/in/rs/mdprogramming/todos/security/UsernameFromTokenProvider.java
+++ src/main/java/in/rs/mdprogramming/todos/security/UsernameFromTokenProvider.java
@@ -1,4 +1,18 @@
 package in.rs.mdprogramming.todos.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import io.jsonwebtoken.Jwts;
+
 public class UsernameFromTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody().getSubject();
+    }
 }
