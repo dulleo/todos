7c7e63472dc809148565718ec84fafce13e40457
dulleo <dulleo@gmail.com>
2019-12-25 08:38:05 +0100

    implemented UserDetails

--- src/main/java/in/rs/mdprogramming/todos/security/UserPrinciple.java
+++ src/main/java/in/rs/mdprogramming/todos/security/UserPrinciple.java
@@ -1,4 +1,129 @@
 package in.rs.mdprogramming.todos.security;
 
-public class UserPrinciple {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import in.rs.mdprogramming.todos.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * UserPrinciple will implement UserDetails. UserPrinciple is not used directly by Spring Security for security purposes.
+ * It simply stores user information which is later encapsulated into Authentication objects.
+ * This allows non-security related user information (such as email addresses, telephone numbers etc) to be stored.
+ */
+public class UserPrinciple implements UserDetails {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    @JsonIgnore
+    private String password;
+    private String email;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserPrinciple(Long id, String firstName, String lastName, String username, String password,
+                         Collection<? extends GrantedAuthority> authorities) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static UserPrinciple build(User user) {
+
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList());
+
+        return new UserPrinciple(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getUsername(),
+                user.getPassword(),
+                authorities);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserPrinciple other = (UserPrinciple) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
