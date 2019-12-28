453e2e0029212d3e7797c6fe2006217c628def38
dulleo <dulleo@gmail.com>
2019-12-28 07:18:04 +0100

    Annotation messages changed

--- src/main/java/in/rs/mdprogramming/todos/dto/UserLoginDTO.java
+++ src/main/java/in/rs/mdprogramming/todos/dto/UserLoginDTO.java
@@ -1,14 +1,15 @@
 package in.rs.mdprogramming.todos.dto;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserLoginDTO {
 
-    @NotNull(message="Property \'username\' is null!")
-    @Size(min=3, max = 50, message = "\'username\' property size must be min = 3 and max = 50 characters.")
-    @Email(message = "Property \'username\' must be an email!")
+    @NotBlank(message="Property \'username\' is null!")
+    @Size(max = 50, message = "\'username\' property size must be max = 50 characters.")
+    @Email(message="Must be an email!")
     private String username;
 
     @NotNull(message="Property \'password\' is null!")
@@ -18,6 +19,8 @@ public class UserLoginDTO {
     public String getUsername() {
         return username;
     }
+
+
 
     public String getPassword() { return password; }
 
