2312cc29fe2b94a7646bf0b502f9e1d76c63029b
dulleo <dulleo@gmail.com>
2019-12-28 03:30:33 +0100

    implementation added

---
+++ src/main/java/in/rs/mdprogramming/todos/exception/ResourceNotFoundException.java
@@ -0,0 +1,15 @@
+package in.rs.mdprogramming.todos.exception;
+
+/**
+ *
+ */
+public class ResourceNotFoundException extends Exception {
+
+    public ResourceNotFoundException() {
+    }
+
+    public ResourceNotFoundException(String meesge) {
+        super(meesge);
+    }
+
+}
