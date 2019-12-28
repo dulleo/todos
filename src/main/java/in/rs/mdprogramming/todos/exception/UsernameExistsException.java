a6810a87f7cffa68ab11f2a43eb3e6eb3044883f
dulleo <dulleo@gmail.com>
2019-12-28 03:31:59 +0100

    implementation added

---
+++ src/main/java/in/rs/mdprogramming/todos/exception/UsernameExistsException.java
@@ -0,0 +1,11 @@
+package in.rs.mdprogramming.todos.exception;
+
+public class UsernameExistsException extends Exception {
+
+    public UsernameExistsException() {
+    }
+
+    public UsernameExistsException(String meesge) {
+        super(meesge);
+    }
+}
