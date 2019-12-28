03d5848448a86746db4e3fd082fab091d971f3be
dulleo <dulleo@gmail.com>
2019-12-28 03:26:43 +0100

    implementation added

--- src/main/java/in/rs/mdprogramming/todos/dto/TodoDTO.java
+++ src/main/java/in/rs/mdprogramming/todos/dto/TodoDTO.java
@@ -1,4 +1,79 @@
 package in.rs.mdprogramming.todos.dto;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
 public class TodoDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Property \'name\' is null or empty!")
+    private String name;
+
+    private String description;
+
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private Date createdAt;
+
+    @NotNull(message = "Property \'targetDate\' is null or empty!")
+    //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private Date targetDate;
+
+    private Boolean isDone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(Date targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public Boolean getIsDone() {
+        return isDone;
+    }
+
+    public void setIsDone(Boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        return "[id=" + id + ", name=" + name + ", description=" + description + ", createdAt=" + createdAt
+                + ", targetDate=" + targetDate + ", isDone=" + isDone + "]";
+    }
+
 }
