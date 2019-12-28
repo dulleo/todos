a1fa7f84fb9fabba89c952d5b386e01b0a7dac3f
dulleo <dulleo@gmail.com>
2019-12-28 03:28:48 +0100

    implementation added

---
+++ src/main/java/in/rs/mdprogramming/todos/exception/InvalidPrincipalException.java
@@ -0,0 +1,14 @@
+package in.rs.mdprogramming.todos.exception;
+
+/**
+ *
+ */
+public class InvalidPrincipalException extends Exception {
+
+    public InvalidPrincipalException() {
+    }
+
+    public InvalidPrincipalException(String message) {
+        super(message);
+    }
+}
