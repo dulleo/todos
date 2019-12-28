a135deb60b27e8100d9fbb2378e60c2defc2495b
dulleo <dulleo@gmail.com>
2019-12-28 03:50:10 +0100

    register method signature added

---
+++ src/main/java/in/rs/mdprogramming/todos/service/RegisterUserServiceInterface.java
@@ -0,0 +1,9 @@
+package in.rs.mdprogramming.todos.service;
+
+import in.rs.mdprogramming.todos.dto.UserRegistrationDTO;
+import in.rs.mdprogramming.todos.exception.UsernameExistsException;
+
+public interface RegisterUserServiceInterface {
+
+    void register(UserRegistrationDTO registerDTO) throws UsernameExistsException;
+}
