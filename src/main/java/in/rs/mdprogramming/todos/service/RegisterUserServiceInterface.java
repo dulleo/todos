218c6012c199a9e73d9a035361299574ad6f4599
dulleo <dulleo@gmail.com>
2019-12-28 04:32:56 +0100

    InvalidRoleException added

--- src/main/java/in/rs/mdprogramming/todos/service/RegisterUserServiceInterface.java
+++ src/main/java/in/rs/mdprogramming/todos/service/RegisterUserServiceInterface.java
@@ -1,9 +1,10 @@
 package in.rs.mdprogramming.todos.service;
 
 import in.rs.mdprogramming.todos.dto.UserRegistrationDTO;
+import in.rs.mdprogramming.todos.exception.InvalidRoleException;
 import in.rs.mdprogramming.todos.exception.UsernameExistsException;
 
 public interface RegisterUserServiceInterface {
 
-    void register(UserRegistrationDTO registerDTO) throws UsernameExistsException;
+    void register(UserRegistrationDTO registerDTO) throws UsernameExistsException, InvalidRoleException;
 }
