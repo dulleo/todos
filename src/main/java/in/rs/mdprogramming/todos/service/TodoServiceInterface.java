76922310c8faf0799e8424ea80f627502f359276
dulleo <dulleo@gmail.com>
2019-12-28 05:07:21 +0100

    Method declarations added

---
+++ src/main/java/in/rs/mdprogramming/todos/service/TodoServiceInterface.java
@@ -0,0 +1,19 @@
+package in.rs.mdprogramming.todos.service;
+
+import in.rs.mdprogramming.todos.dto.TodoDTO;
+import in.rs.mdprogramming.todos.exception.ResourceNotFoundException;
+
+import java.util.List;
+
+public interface TodoServiceInterface {
+
+    void createTodo(TodoDTO todoDTO) throws ResourceNotFoundException;
+
+    List<TodoDTO> getTodos();
+
+    TodoDTO getTodo(Long userId) throws ResourceNotFoundException;
+
+    void editTodo(Long userId, TodoDTO taskDTO) throws ResourceNotFoundException;
+
+    void deleteTodo(Long userId) throws ResourceNotFoundException;
+}
