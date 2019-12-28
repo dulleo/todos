5c2b673012d8bf408abcd209a0f8c37de0c9e786
dulleo <dulleo@gmail.com>
2019-12-28 05:13:46 +0100

    Implementation added

---
+++ src/main/java/in/rs/mdprogramming/todos/RegisterUserController.java
@@ -0,0 +1,27 @@
+package in.rs.mdprogramming.todos;
+
+import in.rs.mdprogramming.todos.dto.UserRegistrationDTO;
+import in.rs.mdprogramming.todos.exception.InvalidRoleException;
+import in.rs.mdprogramming.todos.exception.UsernameExistsException;
+import in.rs.mdprogramming.todos.service.RegisterUserServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class RegisterUserController {
+
+    @Autowired
+    private RegisterUserServiceInterface registerUserService;
+
+    @RequestMapping(method = RequestMethod.POST, path="/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public void registerUser(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO) throws InvalidRoleException, UsernameExistsException {
+        registerUserService.register(userRegistrationDTO);
+    }
+}
