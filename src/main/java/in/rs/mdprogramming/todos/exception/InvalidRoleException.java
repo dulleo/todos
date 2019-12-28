2f44e9ab18fb3e46c18347af3322ae6eccb77c51
dulleo <dulleo@gmail.com>
2019-12-28 04:31:27 +0100

    InvalidRoleException added

---
+++ src/main/java/in/rs/mdprogramming/todos/exception/InvalidRoleException.java
@@ -0,0 +1,11 @@
+package in.rs.mdprogramming.todos.exception;
+
+public class InvalidRoleException extends Throwable {
+
+    public InvalidRoleException() {
+    }
+
+    public InvalidRoleException(String message) {
+        super(message);
+    }
+}
