824f4201cb4050fc7744d254d3e2efad41f143f2
dulleo <dulleo@gmail.com>
2019-12-28 04:36:28 +0100

    Error messages changed and setters removed

--- src/main/java/in/rs/mdprogramming/todos/dto/UserRegistrationDTO.java
+++ src/main/java/in/rs/mdprogramming/todos/dto/UserRegistrationDTO.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Set;
 
-public class UserRegistrationDTO {
+public class UserRegistrationDTO extends UserLoginDTO {
 
     @NotNull(message="Property \'firstName\' is null!")
     @Size(min = 3, max = 50, message = "\'firstName\' property size must be min = 3 and max = 50 characters.")
@@ -21,38 +21,23 @@ public class UserRegistrationDTO {
     @Email(message="Must be an email!")
     private String username;
 
-    @NotNull(message="Property \'roles\' is null!")
+    @NotBlank(message="Property \'roles\' is null or empty!")
     private Set<String> roles;
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public Set<String> getRoles() {
         return this.roles;
     }
 
-    public void setRoles(Set<String> role) {
-        this.roles = role;
-    }
 }
