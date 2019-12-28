d68022ea66bbc6112ed20585a2029f03bad82f21
dulleo <dulleo@gmail.com>
2019-12-28 07:16:28 +0100

    setters removed

--- src/main/java/in/rs/mdprogramming/todos/model/User.java
+++ src/main/java/in/rs/mdprogramming/todos/model/User.java
@@ -41,51 +41,27 @@ public class User {
     }
 
     public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+        return this.id;
     }
 
     public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        return this.firstName;
     }
 
     public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+        return this.lastName;
     }
 
     public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+        return this.password;
     }
 
     public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+        return this.username;
     }
 
     public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
+        return this.roles;
     }
 
     @Override
