32a1f6add5bd0899c3b5858eef37bda0be7b5dc0
dulleo <dulleo@gmail.com>
2019-12-28 05:06:45 +0100

    Entity Task replaced with Todo

--- src/main/java/in/rs/mdprogramming/todos/repository/TodoRepository.java
+++ src/main/java/in/rs/mdprogramming/todos/repository/TodoRepository.java
@@ -1,14 +1,14 @@
 package in.rs.mdprogramming.todos.repository;
 
+import in.rs.mdprogramming.todos.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TodoRepository extends JpaRepository<Task, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    List<Task> findAllByUserId(Long userId);
-    Task findByIdAndUserId(Long userId, Long todoId);
+    List<Todo> findAllByUserId(Long userId);
+    Todo findByIdAndUserId(Long userId, Long todoId);
 }
