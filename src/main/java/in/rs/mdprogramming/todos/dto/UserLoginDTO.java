6e99dea964dfb044c00478d69c0a7b4cc2b058bd
dulleo <dulleo@gmail.com>
2019-12-28 04:35:56 +0100

    Error messages changed and setter removed

--- src/main/java/in/rs/mdprogramming/todos/dto/UserLoginDTO.java
+++ src/main/java/in/rs/mdprogramming/todos/dto/UserLoginDTO.java
@@ -7,29 +7,19 @@ import javax.validation.constraints.Size;
 public class UserLoginDTO {
 
     @NotNull(message="Property \'username\' is null!")
-    @Size(min=3, max = 50, message = "\'username\' property size must be min = 3 and max =  50 characters.")
+    @Size(min=3, max = 50, message = "\'username\' property size must be min = 3 and max = 50 characters.")
     @Email(message = "Property \'username\' must be an email!")
     private String username;
 
     @NotNull(message="Property \'password\' is null!")
-    @Size(min = 8, max = 50, message = "\'password\' property size must be min = 8 and max =  50 characters.")
+    @Size(min = 8, max = 50, message = "\'password\' property size must be min = 8 and max = 50 characters.")
     private String password;
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getPassword() { return password; }
 
     @Override
     public String toString() {
