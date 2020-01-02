1a459f15b2c1a94dc41fc23a1954d9df4d685c17
dulleo <dulleo@gmail.com>
2020-01-02 21:50:38 +0100

    Cross Origin removed

--- src/main/java/in/rs/mdprogramming/todos/controller/TodoController.java
+++ src/main/java/in/rs/mdprogramming/todos/controller/TodoController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-//@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(path = "/api")
 public class TodoController {
