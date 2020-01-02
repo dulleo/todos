c855e846ddb99b08493f71bbc39b36c8039385f7
dulleo <dulleo@gmail.com>
2020-01-02 04:44:36 +0100

    /api added to the path

--- src/main/java/in/rs/mdprogramming/todos/controller/RegisterUserController.java
+++ src/main/java/in/rs/mdprogramming/todos/controller/RegisterUserController.java
@@ -13,7 +13,7 @@ import javax.validation.Valid;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("")
+@RequestMapping("/api")
 public class RegisterUserController {
 
     @Autowired
