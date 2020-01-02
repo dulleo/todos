a601847562cc7bca8a1a2d7e6533b288f66fa9af
dulleo <dulleo@gmail.com>
2020-01-02 21:51:07 +0100

    Cross Origin removed

--- src/main/java/in/rs/mdprogramming/todos/controller/RegisterUserController.java
+++ src/main/java/in/rs/mdprogramming/todos/controller/RegisterUserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class RegisterUserController {
