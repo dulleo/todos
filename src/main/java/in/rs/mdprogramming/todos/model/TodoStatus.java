88437701e2d7d130c53300e99d4cbf30019fa6c9
dulleo <dulleo@gmail.com>
2020-01-03 09:16:01 +0100

    constructor and method fromStatusName added

--- src/main/java/in/rs/mdprogramming/todos/model/TodoStatus.java
+++ src/main/java/in/rs/mdprogramming/todos/model/TodoStatus.java
@@ -1,6 +1,34 @@
 package in.rs.mdprogramming.todos.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import in.rs.mdprogramming.todos.deserializer.TodoStatusDeserializer;
+
+import java.util.Arrays;
+
+@JsonDeserialize(using = TodoStatusDeserializer.class)
 public enum TodoStatus {
-    OPEN,
-    CLOSED
+
+    OPEN("OPEN"),CLOSED("CLOSED");
+
+    private String status;
+
+    TodoStatus(String status) {
+        this.status = status;
+    }
+
+    @JsonCreator
+    public static TodoStatus fromStatusName(String statusName) {
+        for(TodoStatus status : values()) {
+            if(status.status.equals(statusName)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown enum type " + statusName + ", Allowed values are " + Arrays.toString(values()));
+    }
+
+    @Override
+    public String toString() {
+        return status;
+    }
 }
